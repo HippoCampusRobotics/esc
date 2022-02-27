e846b20c97211be62f87ab00cb8d61d16b9863c8
Thies Lennart Alff <lennart.alff@gmail.com>
2022-02-27 20:34:18 +0100

    commander basically completed

--- esc/afro_esc.py
+++ esc/afro_esc.py
@@ -13,19 +13,21 @@ class AfroESC(object):
     INVERT_DIRECTION = (1 << 15)
     ADC_VREF = 5.0
     ADC_RESOLUTION = 1024
+    ADC_VBAT_DIV = 6.45
+    ADC_VBAT_SCALER = ADC_VREF / ADC_RESOLUTION * ADC_VBAT_DIV
 
-    def __init__(self, bus: smbus2.SMBus, address, n_poles=12) -> None:
+    def __init__(self, bus: smbus2.SMBus, address, pole_pairs=6) -> None:
         """Class to communicate with an AfroESC.
 
         Args:
             bus (smbus2.SMBus): Existing bus instance
             address (int): Address of the motor. Usually 0x29 + MOTOR_ID
-            n_poles (int, optional): 12 for Prop NDrive 26-30 800KV. 14 for 
-                older Turnigy motors. Defaults to 12.
+            pole_pairs (int, optional): 6 for Prop NDrive 26-30 800KV. 7 for
+                older Turnigy motors. Defaults to 6.
         """
         self._bus = bus
-        self._address = address
-        self._n_poles = n_poles
+        self.address = address
+        self._pole_pairs = pole_pairs
 
     def init_motor(self):
         self.set_motor_speed(1)
@@ -46,7 +48,7 @@ class AfroESC(object):
             speed_val += self.INVERT_DIRECTION
         speed_val += int(self.MAX_SPEED * speed)
         # ESC expects MSB. SMBus is LSB. So byte order needs to be inversed.
-        self._bus.write_word_data(self._address, self.REG_SET_SPEED,
+        self._bus.write_word_data(self.address, self.REG_SET_SPEED,
                                   ((speed_val << 8) & 0xFF00) |
                                   ((speed_val >> 8) & 0x00FF))
 
@@ -57,7 +59,7 @@ class AfroESC(object):
         Returns:
             int: Number of commutations since last request.
         """
-        data = self._bus.read_i2c_block_data(self._address, self.REG_GET_SPEED,
+        data = self._bus.read_i2c_block_data(self.address, self.REG_GET_SPEED,
                                              2)
         return int(data[0] << 8 | data[1])
 
@@ -69,22 +71,24 @@ class AfroESC(object):
         Returns:
             float: Mechanical revolutions since last request.
         """
-        data = self._bus.read_i2c_block_data(self._address, self.REG_GET_SPEED,
+        data = self._bus.read_i2c_block_data(self.address, self.REG_GET_SPEED,
                                              2)
-        return (data[0] << 8 | data[1]) / self._n_poles
+        return (data[0] << 8 | data[1]) / self._pole_pairs
 
     def get_battery_adc(self) -> int:
-        data = self._bus.read_i2c_block_data(self._address, self.REG_GET_VBAT,
-                                             2)
-        return int(data[0] << 8 | data[1])
+        data = self._bus.read_i2c_block_data(self.address, self.REG_GET_VBAT, 2)
+        return int((data[0] << 8 | data[1]) >> 6)
+
+    def get_battery_voltage(self) -> float:
+        data = self._bus.read_i2c_block_data(self.address, self.REG_GET_VBAT, 2)
+        return ((data[0] << 8 | data[1]) >> 6) * self.ADC_VBAT_SCALER
 
     def get_temperature_adc(self) -> int:
-        data = self._bus.read_i2c_block_data(self._address, self.REG_GET_TEMP,
-                                             2)
+        data = self._bus.read_i2c_block_data(self.address, self.REG_GET_TEMP, 2)
         return int(data[0] << 8 | data[1])
 
     def get_id(self) -> int:
-        return self._bus.read_byte_data(self._address, self.REG_GET_ID)
+        return self._bus.read_byte_data(self.address, self.REG_GET_ID)
 
     def verify_id(self) -> bool:
         """Check if ESC responds to ID request with the expected ID.
